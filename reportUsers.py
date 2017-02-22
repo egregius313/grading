e955ce2f3f7dd8912c8fe3f889ca39cdf6039665
hydrodog <dov.kruger@gmail.com>
2017-02-22 12:45:44 -0500

    creating script to score user participation

---
+++ reportUsers.py
@@ -0,0 +1,34 @@
+import urllib2
+import json
+from subprocess import call
+import sys
+import os
+
+#course = sys.argv[1]
+#assignment = sys.argv[2]
+#index = course + '-' + assignment + '-'
+#print index
+
+home = os.environ['HOME']
+file = open(home + "/github.tok","r")
+token = file.read()
+file.close() 
+print token
+
+#url = 'https://api.github.com/orgs/StevensDeptECE/repos'
+
+#req = urllib2.Request(url)
+#req.add_header('Authorization', 'token %s' % token)
+#res = urllib2.urlopen(req)
+#data = json.load(res)
+
+users = []
+
+users = 'https://api.github.com/orgs/StevensDeptECE/repos'
+
+
+call (["git", "pull"])
+call(["git", "log", "dkruger"])
+		
+print users
+

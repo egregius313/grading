abcb5c1ec49577d5f49d50e3e0c13f655c65dd62
Michael Manis <electricpyrotechnic@gmail.com>
2017-09-09 18:21:23 -0400

    renamed Command class to AssignmentTest, added exact match option, added ability to grade submissions, made file handling more robust, properly clean temp directory, simplified various logic,

--- pycanvasgrader/pycanvasgrader.py
+++ pycanvasgrader/pycanvasgrader.py
@@ -13,13 +13,15 @@ REQUIRED File structure:
 """
 
 # built-ins
-import py
+import glob
 import json
-import re
 import os
+import shutil
+import re
 import subprocess
 from zipfile import ZipFile
 
+import py
 # 3rd-party
 import requests
 
@@ -32,19 +34,14 @@ class TestSkeleton:
     """
     An abstract skeleton to handle testing of a specific group of files
     """
-    def __init__(self, descriptor: str, commands: list, extensions: str=None, file_regex: str=None):
+
+    def __init__(self, descriptor: str, tests: list):
         """
         :param descriptor: The description of this TestSkeleton
-        :param commands: A list of Command objects. These will be run in the order that they are added.
-        :param extensions: Which file extensions this skeleton applies to (Unimplemented)
-        :param file_regex: A regular expression to match files for this skeleton. Combines with extensions (Unimplemented)
+        :param tests: A list of AssignmentTest objects. These will be run in the order that they are added.
         """
-        if not any((extensions, file_regex)):
-            raise ValueError('Either extensions or file_regex must be defined')
         self.descriptor = descriptor
-        self.commands = commands
-        self.extensions = extensions
-        self.file_regex = file_regex
+        self.tests = tests
 
     @classmethod
     def from_file(cls, filename):
@@ -52,46 +49,43 @@ class TestSkeleton:
             data = json.load(skeleton_file)
             try:
                 descriptor = data['descriptor']
-                commands = data['commands']
-                extensions = data.get('extensions')
-                file_regex = data.get('file_regex')
-                if not any((extensions, file_regex)):
-                    raise KeyError
+                tests = data['tests']
             except KeyError:
                 return None
             else:
-                command_list = []
-                for json_dict in commands:
-                    command = Command.from_json_dict(commands[json_dict])
-                    if command is not None:
-                        command_list.append(command)
+                test_list = []
+                for json_dict in tests:
+                    test = AssignmentTest.from_json_dict(tests[json_dict])
+                    if test is not None:
+                        test_list.append(test)
 
-                return TestSkeleton(descriptor, command_list, extensions, file_regex)
+                return TestSkeleton(descriptor, test_list)
 
     def run_tests(self) -> int:
         total_score = 0
-        for count, command in enumerate(self.commands):
+        for count, test in enumerate(self.tests):
             print('\n--Running test %i--' % (count + 1))
-            if command.run_and_match():
-                if command.point_val > 0:
-                    print('--Adding %i points--' % command.point_val)
-                elif command.point_val == 0:
+            if test.run_and_match():
+                if test.point_val > 0:
+                    print('--Adding %i points--' % test.point_val)
+                elif test.point_val == 0:
                     print('--No points set for this test--')
                 else:
-                    print('--Subtracting %i points--' % abs(command.point_val))
-                total_score += command.point_val
+                    print('--Subtracting %i points--' % abs(test.point_val))
+                total_score += test.point_val
             print('--Current score: %i--' % total_score)
         return total_score
 
 
 # TODO Create tests
-class Command:
+class AssignmentTest:
     """
-    An abstract command to be run in a console
+    An abstract test to be run on an assignment submission
     """
-    def __init__(self, command: str, args: str=None, target_file: str=None, ask_for_target: bool=False,
-                 include_filetype: bool=True, print_output: bool=True, output_match: str=None, output_regex: str=None,
-                 negate_match: bool=False, timeout: int=None, point_val: int=0):
+
+    def __init__(self, command: str, args: str = None, target_file: str = None, ask_for_target: bool = False,
+                 include_filetype: bool = True, print_output: bool = True, output_match: str = None, output_regex: str = None,
+                 negate_match: bool = False, exact_match: bool = False, timeout: int = None, point_val: int = 0):
         """
         :param command: The command to be run.
         :param args: The arguments to pass to the command. Use %s to denote a file name
@@ -103,6 +97,7 @@ class Command:
         :param output_regex: A regular expression that the string should match. Combines with output_match.
         If this and output_match are None, then this Command always 'matches'
         :param negate_match: Whether to negate the result of checking output_match and output_regex
+        :param exact_match: Whether the naive string match (output_match) should be an exact check or a substring check
         :param timeout: Time, in seconds, that this Command should run for before timing out
         :param point_val: Amount of points that a successful match is worth (Can be negative)
         """
@@ -117,6 +112,7 @@ class Command:
         else:
             self.output_regex = None
         self.negate_match = negate_match
+        self.exact_match = exact_match
         self.print_output = print_output
         self.timeout = timeout
         self.point_val = point_val
@@ -136,18 +132,19 @@ class Command:
             output_match = json_dict.get('output_match')
             output_regex = json_dict.get('output_regex')
             negate_match = json_dict.get('negate_match')
+            exact_match = json_dict.get('exact_match')
             timeout = json_dict.get('timeout')
             point_val = json_dict.get('point_val')
 
             vars_dict = {'command': command, 'args': args, 'target_file': target_file,
                          'ask_for_target': ask_for_target, 'include_filetype': include_filetype,
                          'print_output': print_output, 'output_match': output_match, 'output_regex': output_regex,
-                         'negate_match': negate_match, 'timeout': timeout, 'point_val': point_val}
+                         'negate_match': negate_match, 'exact_match': exact_match, 'timeout': timeout, 'point_val': point_val}
             args_dict = {}
             for var_name, val in vars_dict.items():
                 if val is not None:
                     args_dict[var_name] = val
-            return Command(**args_dict)
+            return AssignmentTest(**args_dict)
 
     @classmethod
     def target_prompt(cls, command: str):
@@ -177,7 +174,7 @@ class Command:
         args = self.args
         filename = self.target_file
         if self.ask_for_target:
-            filename = Command.target_prompt(self.command)
+            filename = AssignmentTest.target_prompt(self.command)
         if not self.include_filetype:
             filename = os.path.splitext(filename)[0]
         if filename is not None:
@@ -210,21 +207,25 @@ class Command:
                 return True
 
         if self.output_match:
-            if self.output_match in result['stdout']:
+            if self.exact_match:
+                condition = self.output_match == result['stdout']
+            else:
+                condition = self.output_match in result['stdout']
+
+            if condition:
                 print('--Matched string comparison--')
                 if self.negate_match:
                     return False
                 return True
 
-        if self.negate_match:
-            return True
-        return False
+        return self.negate_match
 
 
 class PyCanvasGrader:
     """
     A PyCanvasGrader object; responsible for communicating with the Canvas API
     """
+
     def __init__(self):
         self.token = self.authenticate()
         if self.token == 'none':
@@ -253,7 +254,7 @@ class PyCanvasGrader:
     def close(self):
         self.session.close()
 
-    def courses(self, enrollment_type: str=None) -> list:
+    def courses(self, enrollment_type: str = None) -> list:
         """
         :param enrollment_type: (Optional) teacher, student, ta, observer, designer
         :return: A list of the user's courses as dictionaries, optionally filtered by enrollment_type
@@ -265,7 +266,7 @@ class PyCanvasGrader:
         response = self.session.get(url)
         return json.loads(response.text)
 
-    def assignments(self, course_id: int, ungraded: bool=True) -> list:
+    def assignments(self, course_id: int, ungraded: bool = True) -> list:
         """
         :param course_id: Course ID to filter by
         :param ungraded: Whether to filter assignments by only those that have ungraded work. Default: True
@@ -300,6 +301,13 @@ class PyCanvasGrader:
         response = self.session.get(url)
         return json.loads(response.text)
 
+    def grade_submission(self, course_id, assignment_id, user_id, grade):
+        url = 'https://canvas.instructure.com/api/v1/courses/%i/assignments/%i/submissions/%i/?submission[posted_grade]=%i' \
+              % (course_id, assignment_id, user_id, grade)
+
+        response = self.session.put(url)
+        return json.loads(response.text)
+
 
 def parse_zip(zip_file: str) -> set:
     """
@@ -309,7 +317,7 @@ def parse_zip(zip_file: str) -> set:
     """
     user_ids = set()
 
-    with ZipFile('zips/' + zip_file, 'r') as z:
+    with ZipFile(os.path.join('zips', zip_file), 'r') as z:
         for name in z.namelist():
             if name.count('_') < 3:
                 print('Skipping file: ' + name + '. Invalid filename')
@@ -317,19 +325,23 @@ def parse_zip(zip_file: str) -> set:
                 (username, user_id, unknown, file) = name.split('_', maxsplit=3)
                 user_ids.add(user_id)
 
-                file_path = 'temp/%s/' % user_id
-                os.makedirs(os.path.dirname('temp/%s/' % user_id), exist_ok=True)
+                file_path = os.path.join('temp', user_id, '')
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
                 z.extract(name, path=file_path)
-                os.replace(file_path + name, file_path + file)
+                os.replace(os.path.join(file_path, name), os.path.join(file_path + file))
 
     if len(user_ids) < 1:
         print('Unable to read any files from the zip. Please check the file and try again')
     return user_ids
 
 
-def choose_val(hi_num: int) -> int:
+def choose_val(hi_num: int, allow_zero: bool = False) -> int:
     val = 'none'
-    while not str.isdigit(val) or (int(val) > hi_num or int(val) <= 0):
+
+    while True:
+        if str.isdigit(val) and int(val) <= hi_num:
+            if (allow_zero and int(val) >= 0) or (not allow_zero and int(val) > 0):
+                break
         val = input()
     return int(val)
 
@@ -338,10 +350,7 @@ def choose_bool() -> bool:
     val = 'none'
     while not str.lower(val) in ['y', 'n', 'yes', 'no']:
         val = input()
-    if val in ['y', 'yes']:
-        return True
-    else:
-        return False
+    return val in ['y', 'yes']
 
 
 def parse_skeletons() -> list:
@@ -353,25 +362,47 @@ def parse_skeletons() -> list:
     return skeleton_list
 
 
+def restart_program(grader: PyCanvasGrader):
+    grader.close()
+    clear_tempdir()
+    main()
+    exit(0)
+
+
+def clear_tempdir():
+    try:
+        shutil.rmtree(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'temp'))
+    except Exception:
+        print('An error occurred while removing the "temp" directory. Please delete the directory manually and re-run the program')
+        exit(1)
+
+
 def main():
+    clear_tempdir()
     # Initialize grading session and fetch courses
     grader = PyCanvasGrader()
     course_list = grader.courses('teacher')
     # Have user select course
     print('Choose a course from the following list:')
-    for count, course in enumerate(course_list):
-        print('%i.\t%s (%s)' % (count + 1, course.get('name'), course.get('course_code')))
-    course_choice = choose_val(len(course_list)) - 1  # the plus and minus 1 are to hide the 0-based numbering
+    for count, course in enumerate(course_list, 1):
+        print('%i.\t%s (%s)' % (count, course.get('name'), course.get('course_code')))
+    course_choice = choose_val(len(course_list)) - 1  # the minus 1 is to hide the 0-based numbering
 
     print('Show only ungraded assignments? (y or n):')
     ungraded = choose_bool()
     course_id = course_list[course_choice].get('id')
     assignment_list = grader.assignments(course_list[course_choice].get('id'), ungraded=ungraded)
 
+    if len(assignment_list) < 1:
+        input('No assignments were found. Press enter to restart')
+        grader.close()
+        main()
+        exit(0)
+
     # Have user choose assignment
     print('Choose an assignment to grade:')
-    for count, assignment in enumerate(assignment_list):
-        print('%i.\t%s' % (count + 1, assignment.get('name')))
+    for count, assignment in enumerate(assignment_list, 1):
+        print('%i.\t%s' % (count, assignment.get('name')))
     assignment_choice = choose_val(len(assignment_list)) - 1
     assignment_id = assignment_list[assignment_choice].get('id')
 
@@ -379,7 +410,7 @@ def main():
     print('If you haven\'t already, please download the most current submissions.zip for this assignment:\n' +
           assignment_list[assignment_choice].get('submissions_download_url'))
 
-    input('\nPress enter when this you have placed this zip file into the /zips/ directory')
+    input('\nPress enter when this you have placed this zip file into the "zips" directory')
 
     # Have user choose zip
     invalid_zip = True
@@ -387,13 +418,10 @@ def main():
     while invalid_zip:
         zip_list = []
         print('Choose a zip file to use:')
-        sub = 0  # This is to keep indices visually correct while excluding non-zip files
-        for count, zip_name in enumerate(os.listdir('zips')):
-            if zip_name.split('.')[-1] != 'zip':
-                sub += 1
-                continue
+        for count, zip_name in enumerate(glob.glob(os.path.join('zips', '*.zip')), 1):
+            zip_name = os.path.basename(zip_name)
             zip_list.append(zip_name)
-            print('%i.\t%s' % (count - sub + 1, zip_name))  # Again, the plus and minus 1 are to hide the 0-based numbering
+            print('%i.\t%s' % (count, zip_name))  # Again, the plus and minus 1 are to hide the 0-based numbering
 
         selection = choose_val(len(zip_list)) - 1
         zip_file = zip_list[selection]
@@ -412,10 +440,14 @@ def main():
         main()
         exit(0)
 
+    print('Only grade currently ungraded submissions? (y or n):')
+    ungraded_only = choose_bool()
     # Match the user IDs found in the zip with the IDs in the online submission
     user_submission_dict = {}
     for user_id in user_ids:
         for submission in submission_list:
+            if ungraded_only and submission.get('grader_id') is not None:  # Skip assignments that have been graded already
+                continue
             long_id = submission.get('user_id')
             if str(user_id) in str(long_id):
                 file_path = os.path.join(os.getcwd(), 'temp')
@@ -450,8 +482,8 @@ def main():
         exit(0)
 
     print('Choose a skeleton to use for grading this assignment:')
-    for count, skeleton in enumerate(skeleton_list):
-        print('%i.\t%s' % (count + 1, skeleton.descriptor))
+    for count, skeleton in enumerate(skeleton_list, 1):
+        print('%i.\t%s' % (count, skeleton.descriptor))
     skeleton_choice = choose_val(len(skeleton_list)) - 1
     selected_skeleton = skeleton_list[skeleton_choice]
 
@@ -466,7 +498,7 @@ def main():
     input('Press enter to begin grading\n')
     for cur_user_id in user_submission_dict:
         try:
-            os.chdir('temp/' + str(cur_user_id))
+            os.chdir(os.path.join('temp', str(cur_user_id)))
         except (WindowsError, OSError):
             print('Could not access files for user "%i". Skipping' % cur_user_id)
             continue
@@ -477,12 +509,25 @@ def main():
             score = 0
         action_list = ['Submit this grade', 'Modify this grade', 'Skip this submission', 'Re-grade this submission']
 
-        print('\n--All tests completed--\nGrade for this assignment: %i' % score)
-        print('Choose an action:')
-        for count, action in enumerate(action_list):
-            print('%i.\t%s' % (count + 1, action))
-        action_choice = choose_val(len(action_list)) - 1
-        selected_action = action_list[skeleton_choice]
+        while True:
+            print('\n--All tests completed--\nGrade for this assignment: %i' % score)
+            print('Choose an action:')
+            for count, action in enumerate(action_list, 1):
+                print('%i.\t%s' % (count, action))
+            action_choice = choose_val(len(action_list)) - 1
+            selected_action = action_list[action_choice]
+
+            if selected_action == 'Submit this grade':
+                grader.grade_submission(course_id, assignment_id, cur_user_id, score)
+                print('Grade submitted')
+                break
+            elif selected_action == 'Modify this grade':
+                print('Enter a new grade for this submission:')
+                score = choose_val(1000, allow_zero=True)
+            elif selected_action == 'Skip this submission':
+                break
+            elif selected_action == 'Re-grade this submission':
+                score = selected_skeleton.run_tests()
 
     print('done')
 
